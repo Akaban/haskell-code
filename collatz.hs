d32039c1fc45380212da63b99a356ab34a1bd683
Bryce Tichit <tichit.bryce@gmail.com>
2017-11-06 16:21:06 +0100

    Delete collatz.hs

--- collatz.hs
+++
@@ -1,7 +0,0 @@
-collatz :: Int -> [Int]
-collatz n = docollatz n acc
-    where docollatz n acc 
-        | n == 1 = 1 : acc
-        | even n = docollatz en (en:acc)
-        | otherwise = dollatz on (on:acc)
-            where en,on = en/2,
