3795f17a9627fd13e84ca2e44e7d9f9689eb7e1b
Bryce Tichit <tichit.bryce@gmail.com>
2017-12-28 14:49:59 +0100

    implement activation functions in NeuronNet

--- HaskellML/Neuronal.hs
+++ HaskellML/Neuronal.hs
@@ -8,11 +8,19 @@ data NeuronNet =
     NeuronNet { hiddenLayerNumber :: Int ,
       outputDimension :: Int ,
       inputDimension :: Int,
-      weights :: [Mat] }
+      weights :: [Mat],
+      activation :: Double -> Double }
+
+-- Activation functions
+
+sigmoid :: (Floating a) => a -> a
+sigmoid t = 1 / (1 + exp (-t))
 
 computeNeuronal :: Mat -> NeuronNet -> Mat
-computeNeuronal input =
-    (flip (*) input) . composeMatrixFromList . weights 
-        where composeMatrixFromList = foldl1 (*) 
+computeNeuronal input network =
+    foldl (\computed weightInput
+            -> activation' $
+                weightInput * computed) input (weights network)
+    where activation' = mapCol (\_ -> activation network) 1 
 
 neuronalNetworkApp = flip computeNeuronal
